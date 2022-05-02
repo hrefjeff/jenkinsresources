1cd687c1f25685872d2addae52772e2c0ea16e7f
Jeffrey Allen <2584585+hrefjeff@users.noreply.github.com>
2022-05-02 08:51:27 -0500

    Update credential-test.groovy

--- credential-test.groovy
+++ credential-test.groovy
@@ -1,15 +1,20 @@
 pipeline {
 
   agent {
-    label 'master'
+    label any
+    // label label 'master'
   }
   
   stages {
     stage('Pull from somewhere HTTPS') {
       steps {
-        
-        withCredentials('something here')
-      
+              checkout([$class: 'GitSCM', 
+                branches: [[name: '*/main']],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [[$class: 'CleanCheckout']],
+                submoduleCfg: [], 
+                userRemoteConfigs: [[url: 'https://github.com/naiveskill/devops.git']]])
+              sh "ls -ltr"
       }
     }
   }
